2569261cd7593b8e295f215eef2a6de83e3e4747
irtiza-chowdhury <irtizachowdhury5@gmail.com>
2022-11-21 22:12:06 +0600

    unnecessary file deleted

--- js/modalbs.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('exampleModal')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
